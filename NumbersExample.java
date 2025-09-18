bb751d7f3870e6e29d74a48343013bba483601fd
sathiyapriyan2007 <sathyapriyanr2007@gmail.com>
2025-09-18 10:25:07 +0530

    Add files via upload

---
+++ NumbersExample.java
@@ -0,0 +1,20 @@
+public class NumbersExample {
+    public static void main(String[] args) {
+        int num1 = 12;
+        int num2 = 4;
+
+        // Operations
+        int sum = num1 + num2;
+        int difference = num1 - num2;
+        int product = num1 * num2;
+        double quotient = (double) num1 / num2;
+
+        // Display results
+        System.out.println("First number = " + num1);
+        System.out.println("Second number = " + num2);
+        System.out.println("Sum = " + sum);
+        System.out.println("Difference = " + difference);
+        System.out.println("Product = " + product);
+        System.out.println("Quotient = " + quotient);
+    }
+}
